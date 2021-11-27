b7062fb722e5d7fec87a85a481a7a2561534c3a0
Soviaat2 <95125341+Soviaat2@users.noreply.github.com>
2021-11-27 11:03:42 +0100

    Create index.html.h

---
+++ index.html.h
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html> 
+    <head> 
+        <meta charset="UTF-8">
+        <meta name="description" content="The DANIEL TECH">
+        <title>Asdasdasd🕳</title>
+    </head>
+    <body style="background-color: rgb(54, 54, 54);">
+        <form action="https://www.youtube.com">
+            <input type="submit" value="Go to YouTube" />
+        </form>
+    </form>
+        
+    </body>
+</html>
